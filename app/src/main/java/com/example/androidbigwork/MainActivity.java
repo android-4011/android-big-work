db6d01e1d6de628b06bee4e9c1c5353a1fa8b04f
mumuwang <1259149256@qq.com>
2019-11-25 16:45:24 +0800

    ljc_first_test

--- app/src/main/java/com/example/androidbigwork/MainActivity.java
+++ app/src/main/java/com/example/androidbigwork/MainActivity.java
@@ -2,7 +2,22 @@ package com.example.androidbigwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
+
+import com.haibin.calendarview.Calendar;
+import com.haibin.calendarview.MonthView;
+
+
+
+
+
+/**
+ * 首先继承月视图，假如我们想实现高仿魅族的日历
+ */
+
 
 public class MainActivity extends AppCompatActivity {
 
