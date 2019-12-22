4218ce829facbbecfcc08261fc6d1c523fe04e35
iamchf <1767419003@qq.com>
2019-12-22 14:08:54 +0800

    有错误，闪退

--- app/src/main/java/com/example/androidbigwork/MainActivity.java
+++ app/src/main/java/com/example/androidbigwork/MainActivity.java
@@ -1,76 +1,45 @@
 package com.example.androidbigwork;
 
+
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.Window;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-import com.haibin.calendarview.Calendar;
 import com.haibin.calendarview.CalendarView;
 import com.haibin.calendarview.MonthView;
-/**
- * 首先继承月视图，假如我们想实现高仿魅族的日历
- */
-public class MainActivity extends AppCompatActivity {
 
+
+
+public class MainActivity extends AppCompatActivity {
+    /*
     CalendarView calendarView;
     TextView textView2;
     TextView textView1;
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        Button button2=(Button)findViewById(R.id.button2);
-        Button button3=(Button)findViewById(R.id.button3);
-        Button button4=(Button)findViewById(R.id.button4);
-        button2.setOnClickListener( (View.OnClickListener) new button2_Click());
-        button3.setOnClickListener( (View.OnClickListener) new button3_Click());
-        button4.setOnClickListener( (View.OnClickListener) new button4_Click());
-        textView2=findViewById(R.id.textView2);
-        textView1=findViewById(R.id.textView);
-        calendarView=findViewById(R.id.calendarView);
-        calendarView.setOnMonthChangeListener(new CalendarView.OnMonthChangeListener() {
-            @Override
-            public void onMonthChange(int year, int month) {
-                textView2.setText(month);
-            }
-        });
+        /*
+        textView1 = findViewById(R.id.textView1);//年份
+        textView2 = findViewById(R.id.textView2);//月份
+
+        calendarView = findViewById(R.id.calendarView);
+        textView1.setText(String.valueOf(calendarView.getCurYear()));
+        textView2.setText(String.valueOf(calendarView.getCurMonth()));
+
         calendarView.setOnYearChangeListener(new CalendarView.OnYearChangeListener() {
             @Override
             public void onYearChange(int year) {
-                textView1.setText(year);
+                textView1.setText(String.valueOf(year));
             }
         });
-    }
 
-    private class button2_Click implements View.OnClickListener{
-        @Override
-        public void onClick(View v) {
-            Toast.makeText(MainActivity.this,"当前已在月视图",Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private class button3_Click implements View.OnClickListener{
-        @Override
-        public void onClick(View v) {
-            Intent i=new Intent(MainActivity.this,Main2Activity.class);
-            startActivity(i);
-        }
-    }
-
-    private class button4_Click implements View.OnClickListener{
-        @Override
-        public void onClick(View v) {
-
-        }
+        calendarView.setOnMonthChangeListener(new CalendarView.OnMonthChangeListener() {
+            @Override
+            public void onMonthChange(int year, int month) {
+                textView2.setText(String.valueOf(month));
+            }
+        });
+        */
     }
 }
